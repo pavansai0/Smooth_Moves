6808a06b0fe9fee71200c9aa285e274ac32f9d03
Pavan <pavansai052001@gmail.com>
2023-06-28 10:47:20 -0700

    firebase fixed

---
+++ web/firebase.js
@@ -0,0 +1,14 @@
+// Replace the placeholder values with your actual Firebase configuration
+var firebaseConfig = {
+    apiKey: 'AIzaSyAgPh7LKYMTIsm9CmRgfUrsItX3QoXlKmU',
+    appId: '1:691193849940:web:25a4d81eec49f9fcf565c1',
+    messagingSenderId: '691193849940',
+    projectId: 'smooth-moves-8b940',
+    authDomain: 'smooth-moves-8b940.firebaseapp.com',
+    databaseURL: 'https://smooth-moves-8b940-default-rtdb.firebaseio.com',
+    storageBucket: 'smooth-moves-8b940.appspot.com',
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
